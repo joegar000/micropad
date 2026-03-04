02d63c7891d7de4f8ffb7e9e694ccbd1ce6a7029
David Garcia <joegar000@gmail.com>
2026-03-03 18:50:13 -0600

    Credit where credit is due

--- apps/client/lib/gridstack-react/index.ts
+++ apps/client/lib/gridstack-react/index.ts
@@ -30,3 +30,9 @@ export { GridStackHandleReInitializer } from "./grid-stack-handle-re-initializer
 
 export type { GridStackDragInItemProps } from "./grid-stack-drag-in-item";
 export { GridStackDragInItem } from "./grid-stack-drag-in-item";
+
+
+
+/*
+ * Original source: https://www.npmjs.com/package/@iqmo/gridstack-react
+ */
