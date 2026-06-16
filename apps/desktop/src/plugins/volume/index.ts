9b95c2d120294a558843cb1fd99f1f4eeaa60a01
David Garcia <joegar000@gmail.com>
2026-06-16 16:33:58 -0600

    Use loudness instead of easy-volume
    Allegedly it's faster

--- apps/desktop/src/plugins/volume/index.ts
+++ apps/desktop/src/plugins/volume/index.ts
@@ -1,24 +1,37 @@
-import { getVolume, setVolume } from "easy-volume";
 import { PluginAPI } from "../registry.js";
 import { SliderViewModel } from "micropad-widgets";
+import loudness from "loudness";
 
 export default function volume(api: PluginAPI) {
     api.createPlugin('volume', packet => {
+        let volume: number;
         const masterVolume = SliderViewModel.fromConfig({
             pluginName: 'volume',
             title: 'Volume',
             widgetName: 'masterVolume',
         });
         masterVolume.onChange(api.ws, async (data, context) => {
+            if (volume === data.value) return;
             try {
-                await setVolume(data.value);
+                await loudness.setVolume(data.value);
+                volume = data.value;
                 masterVolume.emitChange(api.ws, { value: data.value }, context);
             } catch (e) {
                 console.error('Failed to set volume:', e);
-                masterVolume.emitChange(api.ws, { value: await getVolume() }, context);
+                masterVolume.emitChange(api.ws, { value: volume }, context);
             }
         });
 
         packet.addWidgets(masterVolume);
+        
+        const keepVolumeSynced = async () => {
+            const newV = await loudness.getVolume();
+            if (newV !== volume) {
+                volume = newV;
+                masterVolume.emitChange(api.ws, { value: volume });
+            }
+            setTimeout(keepVolumeSynced, 200);
+        }
+        keepVolumeSynced();
     });
 }
