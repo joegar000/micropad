aefaf25bc638e0dd8475d5a77619f5ec39419cdd
David Garcia <joegar000@gmail.com>
2026-09-12 16:42:46 -0500

    Update server plugin context to use brige

--- app/server/plugins/context.ts
+++ app/server/plugins/context.ts
@@ -1,72 +1,48 @@
 import { Socket } from "socket.io";
 import type { IRuntimeState, IPreservedState } from "micropad-sdk/shared";
-import { SocketValue } from "./socket-value.ts";
 import { DB } from "../db/db.ts";
 import { reaction } from "mobx";
 import { clone } from "es-toolkit/compat";
+import { bridge } from "micropad-sdk/server";
 
-abstract class GenericSocketState {
-  protected readonly prefix: string;
-  protected readonly socket: Socket;
-  protected readonly target: Record<string, any>;
+class RuntimeState implements IRuntimeState {
   [key: string]: any;
 
-  constructor(socket: Socket, prefix: string) {
-    this.socket = socket;
-    this.target = {};
-    this.prefix = prefix;
-    new Proxy<Record<string | symbol, any>>(this, { get: this.get, set: this.set });
-  }
-
-  protected set(_target: this, property: string | symbol, newValue: any) {
-    if (typeof property === 'symbol')
-      throw TypeError(`Cannot set symbol properties on ${this.prefix}`);
-    if (!(property in this.target)) {
-      this.target[property] = new SocketValue(this.socket, this.prefix, property, newValue);
-    } else {
-      (this.target[property] as SocketValue<any>).value = newValue;
-    }
-    return true;
-  }
-
-  protected get(_target: this, property: string | symbol) {
-    if (typeof property === 'symbol')
-      return undefined;
-    if (!(property in this.target)) {
-      this.target[property] = new SocketValue(this.socket, this.prefix, property);
-    }
-    return (this.target[property] as SocketValue<any>).value;
-  }
-
-  toJSON() {
-    return this.target;
+  constructor(socket: Socket, pluginName: string) {
+    const { data, attach } = bridge(`runtime:${pluginName}`);
+    attach(socket);
+    return data;
   }
 }
 
-
-class RuntimeState extends GenericSocketState implements IRuntimeState {
+class PreservedState implements IPreservedState {
+  enabled: boolean = true;
   [key: string]: any;
 
-
   constructor(socket: Socket, pluginName: string) {
-    super(socket, `runtime:${pluginName}`);
-  }
-}
-
-class PreservedState extends GenericSocketState implements IPreservedState {
-  enabled: boolean;
-  [key: string]: any;
-  protected pluginDB: any;
-
-  constructor(socket: Socket, pluginName: string) {
-    super(socket, `preserved:${pluginName}`);
     DB.data.pluginState ??= {};
     DB.data.pluginState[pluginName] ??= {};
+
+    const pluginState = DB.data.pluginState[pluginName];
+    const { data, attach } = bridge(`db:${pluginName}`);
+    for (const key of Object.keys(data)) {
+      delete data[key];
+    }
+    for (const key of Object.keys(pluginState)) {
+      data[key] = pluginState[key];
+    }
+    data.enabled ??= true;
+
     reaction(
-      () => JSON.stringify(this.target),
-      () => (DB.data.pluginState[pluginName] = clone(this.target))
+      () => JSON.stringify(data),
+      () => {
+        DB.data.pluginState![pluginName] = clone(data);
+      }
     );
-    this.enabled = true;
+
+    attach(socket);
+
+    return data as IPreservedState;
   }
 }
 
